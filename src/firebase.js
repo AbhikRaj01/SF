cc119319f6b1adee168283f871d5acdf61c58ba5
AbhikRaj01 <abhikraj948@gmail.com>
2025-03-22 00:45:23 +0530

    HomePage Made

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBZXchjEIlK8zPgCNcKF--fbSkAHgGdbOY",
-  authDomain: "skillforge-be2f7.firebaseapp.com",
-  projectId: "skillforge-be2f7",
-  storageBucket: "skillforge-be2f7.firebasestorage.app",
-  messagingSenderId: "389955536913",
-  appId: "1:389955536913:web:f406f8483ebd03ee1d09b0",
-  measurementId: "G-HE4V3E88FQ"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
